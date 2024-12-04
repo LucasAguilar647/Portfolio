7e14e7d0e7897481dd8a816cf7d4291bd30cbcec
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-12-04 16:47:14 -0300

    add: añadi las imagenene con link de contacto

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -8,11 +8,22 @@ export const Presentation = () => {
     <div className="presentation-content">
       <section className="img-section">
         <img src={perfil} alt="Foto de perfil" />
+        <div className="social-links">
+          <a href="https://www.linkedin.com/in/lucas-aguilar-5b5412249/" target="_blank" rel="noopener noreferrer">
+            <img src="src/assets/icons/linkedin.png" alt="LinkedIn" />
+          </a>
+          <a href="https://github.com/LucasAguilar647" target="_blank" rel="noopener noreferrer">
+            <img src="src/assets/icons/github (2).png" alt="GitHub" />
+          </a>
+          <a href="mailto:lucas.aguilar@email.com" target="_blank" rel="noopener noreferrer">
+            <img src="src/assets/icons/gmail.png" alt="Gmail" />
+          </a>
+        </div>
       </section>
       <div>
         <h1>Soy Lucas Aguilar</h1>
         <p>
-        {textPresentation}
+          {textPresentation}
         </p>
       </div>
     </div>

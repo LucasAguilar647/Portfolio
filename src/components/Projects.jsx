d1730450989baf93081e191e949e740d85753709
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-12-04 16:03:09 -0300

    add: seccion de proyectos

---
+++ src/components/Projects.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/Projects.css'
+
+export const Projects = () => {
+  return (
+    <>
+    <h1>Proyectos</h1>
+    <div>
+        
+
+    </div>
+    
+    </>
+  )
+}

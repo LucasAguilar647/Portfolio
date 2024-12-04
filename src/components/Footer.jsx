f0c406b3d740824dba804ca64e970445c1ffbbd9
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-12-04 16:35:55 -0300

    add + fix : agregeue el footer y formatie mas docs

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <p className="footer-text">© 2024 Mi Página Web. Todos los derechos reservados.</p>
+            </div>
+        </footer>
+    );
+};
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,17 +3,20 @@ import { NavBar } from '../components/NavBar';
 import { Presentation } from '../components/Presentation';
 import { Herramientas } from '../components/Herramientas';
 import { Projects } from '../components/Projects';
+import { Footer } from '../components/Footer';
 
 export const HomePage = () => {
-  return (
-    <>
-    
-        <Presentation/>
-        <Herramientas/>
-        <Projects/>
-    
-    </>
-  )
+    return (
+        <>
+
+
+            <Presentation />
+            <Herramientas />
+            <Projects />
+            <Footer />
+
+        </>
+    )
 }
 
 export default HomePage;

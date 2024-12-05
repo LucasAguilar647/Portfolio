00fa95458f8573433f2a46e41a195a26d30fc1b5
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-12-05 13:02:00 -0300

    add: resume

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -4,6 +4,13 @@ import perfil from '../assets/images/perfil.jpeg';
 import { textPresentation } from '../constantes/constantes';
 
 export const Presentation = () => {
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = 'src/assets/documents/LucasAguilar.cv.pdf';
+    link.download = 'Lucas Aguilar.pdf';
+    link.click();
+  };
+
   return (
     <div className="presentation-content">
       <section className="img-section">
@@ -18,18 +25,21 @@ export const Presentation = () => {
           <a href="mailto:lucas.aguilar@email.com" target="_blank" rel="noopener noreferrer">
             <img src="src/assets/icons/gmail.png" alt="Gmail" />
           </a>
+          <button className='social-button' onClick={handleDownload}>
+            <img src="src/assets/icons/resume.png" alt="Resume" />
+          </button>
+          
         </div>
       </section>
       <div>
-      <div align="center">
+        <div align="center">
           <img
-            src="https://readme-typing-svg.herokuapp.com?color=%D6BD98&size=32&center=true&vCenter=true&width=600&height=50&lines=Hola,soy Lucas Aguilar"
+            src="https://readme-typing-svg.herokuapp.com?color=%D6BD98&size=32&center=true&vCenter=true&width=600&height=50&lines=Hola,soy Lucas Aguilar!;Desarrollador+Frontend+Jr"
             alt="Typing animation"
           />
         </div>
         <p>{textPresentation}</p>
       </div>
-
     </div>
   );
 };
--- src/constantes/constantes.jsx
+++ src/constantes/constantes.jsx
@@ -1,6 +1,4 @@
-export const textPresentation= "Estudiante avanzado de la Licenciatura en Sistemas, próximo a graduarme como Técnico Universitario en Informática. Experiencia en desarrollo de software con tecnologías como React, JavaScript, Java, Kotlin y Assembler.Especializado en desarrollo web frontend y con capacidad para participar en todas las etapas del ciclo de vida del software. Nivel avanzado de inglés y fuerte orientación al aprendizaje continuo y trabajo en equipo."
-
-
+export const textPresentation = "Soy un estudiante avanzado de la Licenciatura en Sistemas, próximo a graduarme como Técnico Universitario en Informática. Tengo experiencia en desarrollo de software utilizando tecnologías como React, JavaScript, Java, Kotlin y Assembler. Estoy especializado en desarrollo web frontend y tengo la capacidad de participar en todas las etapas del ciclo de vida del software. Poseo un nivel avanzado de inglés y una fuerte orientación hacia el aprendizaje continuo y el trabajo en equipo."
 
 
 export default {textPresentation}
